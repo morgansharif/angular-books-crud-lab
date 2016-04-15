faaf34725352861791737cdeee28a7a803c6fea3
morgansharif <morgan.sharif@gmail.com>
2016-04-15 12:27:56 -0700

    listed books by title

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -1,7 +1,20 @@
 angular.module('libraryApp')
   .controller('BooksIndexController', BooksIndexController);
 
+var endpoint = 'https://super-crud.herokuapp.com/books';
+
 BooksIndexController.$inject=['$http'];
 function BooksIndexController( $http ) {
   var vm = this;
+
+
+    $http({
+      method: 'GET',
+      url: endpoint,
+    }).then(function successCallback(response) {
+      vm.books = response.data.books;
+    }, function errorCallback(response) {
+      console.log('There was an error getting the data', response);
+    });
+
 }
